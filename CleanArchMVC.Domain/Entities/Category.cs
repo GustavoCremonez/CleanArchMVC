2b6491455d6d6b5b4f2f3042a947f74f9e9e874a
Gustavo Cremonez <aparecido2332001@gmail.com>
2023-08-01 21:56:50 -0300

    Adding: domain entities and an exceptions' validation handler

---
+++ CleanArchMVC.Domain/Entities/Category.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchMVC.Domain.Validation;
+
+namespace CleanArchMVC.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public string Name { get; private set; }
+
+        public ICollection<Product> Products { get; set; }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name. Too short, minimum 3 characters");
+
+            Name = name;
+        }
+    }
+}
---
+++ CleanArchMVC.Domain/Entities/Product.cs
@@ -0,0 +1,56 @@
+﻿using CleanArchMVC.Domain.Validation;
+
+namespace CleanArchMVC.Domain.Entities
+{
+    public sealed class Product : Entity
+    {
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        public Product(int id, string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(Id < 0, "Invalid Id value");
+            Id = id;
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public decimal Price { get; private set; }
+
+        public int Stock { get; private set; }
+
+        public string Image { get; private set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+
+        private void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+            ValidateDomain(name, description, price, stock, image);
+            CategoryId = categoryId;
+        }
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name. Too short, minimum 3 characters");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid description. Description is required");
+            DomainExceptionValidation.When(description.Length < 5, "Invalid description. Too short, minimum 5 characters");
+            DomainExceptionValidation.When(price < 0, "Invalid price value");
+            DomainExceptionValidation.When(stock < 0, "Invalid stock value");
+            DomainExceptionValidation.When(image.Length > 250, "Invalid image name. Too long, maximum 250 characters");
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+    }
+}
