416a63b6406e8f8ed8f285c40ddc16660d90a026
Gustavo Cremonez <aparecido2332001@gmail.com>
2023-08-02 22:26:29 -0300

    Adding: Contract interfaces for repositories

---
+++ CleanArchMVC.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchMVC.Domain.Entities;
+
+namespace CleanArchMVC.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+
+        Task<Category> GetCategoryByIdAsync(int id);
+
+        Task<Category> CreateAsync(Category category);
+
+        Task<Category> UpdateAsync(Category category);
+
+        Task<Category> RemoveAsync(Category category);
+    }
+}
---
+++ CleanArchMVC.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchMVC.Domain.Entities;
+
+namespace CleanArchMVC.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+
+        Task<Product> GetProductByIdAsync(int id);
+
+        Task<Product> GetProductByCategoryAsync(int categoryId);
+
+        Task<Product> CreateAsync(Product product);
+
+        Task<Product> UpdateAsync(Product product);
+
+        Task<Product> RemoveAsync(Product product);
+    }
+}
