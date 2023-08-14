fd19c46733d57c827d4e74f54c8c3083c0f2789d
Gustavo Cremonez <aparecido2332001@gmail.com>
2023-08-13 23:01:43 -0300

    Adding: concrete class implementation of entity repository interfaces

---
+++ CleanArchMVC.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,56 @@
+﻿using CleanArchMVC.Domain.Entities;
+using CleanArchMVC.Domain.Interfaces;
+using CleanArchMVC.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMVC.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            _context.Add(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            _context.Update(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<Category> RemoveAsync(Category category)
+        {
+            _context.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryByIdAsync(int id)
+        {
+            Category? entity = await _context.Categories.FindAsync(id);
+
+            if (entity != null)
+                return entity;
+
+            throw new ApplicationException("Não foi encontrado a categoria desejada, tente novamente!");
+        }
+    }
+}
---
+++ CleanArchMVC.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,68 @@
+﻿using CleanArchMVC.Domain.Entities;
+using CleanArchMVC.Domain.Interfaces;
+using CleanArchMVC.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMVC.Infra.Data.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _context.Add(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            _context.Update(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> RemoveAsync(Product product)
+        {
+            _context.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> GetProductByCategoryAsync(int id)
+        {
+            Product? entity = await _context.Products
+                .Include(p => p.Category)
+                .SingleOrDefaultAsync(p => p.Id == id);
+
+            if (entity != null)
+                return entity;
+
+            throw new ApplicationException("Não foi encontrado o produto desejado, tente novamente!");
+        }
+
+        public async Task<Product> GetProductByIdAsync(int id)
+        {
+            Product? entity = await _context.Products.FindAsync(id);
+
+            if (entity != null)
+                return entity;
+
+            throw new ApplicationException("Não foi encontrado o produto desejado, tente novamente!");
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
