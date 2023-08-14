a85fbd41097531057ebcdcecfdf9082920d0eb11
Gustavo Cremonez <aparecido2332001@gmail.com>
2023-08-13 22:59:40 -0300

    Adding: entities configuration in context of database

---
+++ CleanArchMVC.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchMVC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMVC.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(120).IsRequired();
+            builder.Property(x => x.Price).HasPrecision(10, 2);
+
+            builder.HasOne(e => e.Category)
+                .WithMany(e => e.Products)
+                .HasForeignKey(e => e.CategoryId);
+        }
+    }
+}
